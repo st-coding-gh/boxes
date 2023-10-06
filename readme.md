2a3c1aaf6ab7f8f4d72b9de6baceacc0c3e22b1e
st-coding-gh <st-coding@yandex.ru>
2023-10-06 21:54:58 +0300

    added description to test json

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # Boxes
 
 an application to manage storage on shelves
+
+## temporary url:
+
+http://45.90.35.211:3333/
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.get('/', (req, res) => {
   res.json({
     project: 'boxes',
     stack: ['express', 'webpack', 'react'],
+    description: 'an application to manage storage on shelves',
   })
 })
 
