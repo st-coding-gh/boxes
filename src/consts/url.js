960ea45d537c4146c6f51bf61dc8ad435d05058f
st-coding-gh <st-coding@yandex.ru>
2023-10-11 01:03:55 +0300

    test

--- src/consts/url.js
+++ src/consts/url.js
@@ -1,17 +1,17 @@
-export default function () {
-  return {
-    getAll: 'http://localhost:3333/database/get-all',
-    createItem: 'http://localhost:3333/database/create-items',
-    deleteItem: 'http://localhost:3333/database/delete-item',
-    updateItem: 'http://localhost:3333/database/update-item',
-  }
-}
-
 // export default function () {
 //   return {
-//     getAll: '/database/get-all',
-//     createItem: '/database/create-items',
-//     deleteItem: '/database/delete-item',
-//     updateItem: '/database/update-item',
+//     getAll: 'http://localhost:3333/database/get-all',
+//     createItem: 'http://localhost:3333/database/create-items',
+//     deleteItem: 'http://localhost:3333/database/delete-item',
+//     updateItem: 'http://localhost:3333/database/update-item',
 //   }
 // }
+
+export default function () {
+  return {
+    getAll: '/database/get-all',
+    createItem: '/database/create-items',
+    deleteItem: '/database/delete-item',
+    updateItem: '/database/update-item',
+  }
+}
