428a7f10672600a15e384d13136fad9570781c44
st-coding-gh <st-coding@yandex.ru>
2023-11-30 20:54:24 +0300

    fix routing

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import fs from 'node:fs'
+import path from 'node:path'
 const app = express()
 const port = 3333
 const paths = ['/:controller', '/:controller/:action']
@@ -8,6 +9,8 @@ async function router(req, res) {
   const route = {}
   route.controller = req.params.controller
   route.action = req.params.action ?? 'index'
+
+  console.log(route)
 
   const file = `./controllers/${route.controller}/${route.action}.js`
   if (!fs.existsSync(file)) {
@@ -27,7 +30,14 @@ app.use(function (req, res, next) {
   )
   next()
 })
+
 app.use(express.static('static'))
+
+app.get('/list', function (req, res) {
+  const url = path.resolve('./static/index.html')
+  res.sendFile(url)
+})
+
 app.get('/:controller', router)
 app.get('/:controller/:action', router)
 
